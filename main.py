f4a07cdc35b52440591f5d1edddc86d8bae81ecc
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-04-20 01:34:59 +0530

    basic setup done

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import struct
 import textwrap
 
 
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\n Ethernet Frame : ')
+        print('Destination : {} , Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+
+
 # return formatted mac address
 def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
@@ -14,3 +24,6 @@ def get_mac_addr(bytes_addr):
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+
+main()
